432fd9d5711394017da0b9be72e57cac01ab920a
Ciphr7 <gil@ciphr7.com>
2021-10-18 16:01:46 -0600

    messing with the footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,50 @@
 <template>
+   <v-navigation-drawer class="pa-2"  v-model="Drawer2" app>
+    <v-list-item >
+      <v-list-item-content class="white--text d-flex justify-center">
+        <v-img max-height="70" max-width="70" src="../assets/logo.png"></v-img>
+        <v-list-item-title class="text-h6 white--text d-flex justify-center">
+          Trip Planner
+        </v-list-item-title >
+        <v-list-item-subtitle class="white--text d-flex justify-center">
+          New Trip - Going today?
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
 
-  <v-footer dark padless class="red lighten-1  ">
+    <v-divider class="red lighten-1"></v-divider>
+
+    <v-card  class="px-2" elevation="9" >
+      <v-checkbox
+        pa-5
+        v-model="checkbox"
+        :label="`Set Origin to My GPS Location`"
+      ></v-checkbox>
+
+       <v-text-field
+      label="Origin"
+      :rules="rules"
+      hide-details="auto"
+      class="px-2"
+    ></v-text-field>
+    <v-text-field class="px-2" label="Destination #Dallas, tx"></v-text-field>
+      <v-btn block>
+        Run Trip
+      </v-btn>
+      <v-card-text>Trip Options</v-card-text>
+      <v-select :items="r_items" filled label="Practical"></v-select>
+      <v-checkbox pa-5 v-model="checkbox" :label="`Close Borders`"></v-checkbox>
+      <v-checkbox pa-5 v-model="checkbox" :label="`Avoid Toll`"></v-checkbox>
+    </v-card>
+
+   
+  </v-navigation-drawer>
+</template>
+<template>
+
+  <v-app-bar dark padless class="blue lighten-1  ">
+    <v-app-bar-nav-icon @click="Drawer2 = !Drawer2"></v-app-bar-nav-icon>
+
     <v-card class=" mx-auto my-3" color="secondary" width="70%">
       <v-card-text class=" d-flex justify-center">
         <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
@@ -21,14 +65,16 @@
         {{ new Date().getFullYear() }} — <strong>TruckMiles 2021</strong>
       </v-card-text>
     </v-card>
-  </v-footer>
+  </v-app-bar>
 </template>
+
 <script>
 
 export default {
   name: "Footer",
 
   data: () => ({
+     Drawer2: null,
     icons: ["mdi-truck", "mdi-map", "mdi-check"],
   }),
 };
