323e10aa6622bcc6ce526367ff887bba359575a4
ciphr7 <gil@ciphr7.com>
2021-11-07 05:37:24 -0600

    mapSecurity

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 import x5GMaps from 'x5-gmaps'
 // Option 1: Just your key
-Vue.use(x5GMaps, 'AIzaSyD58iVDx399ThN50pkEl3kDHi5tGRH3XCU')
+Vue.use(x5GMaps, '')
 // Option 2: With libraries
-Vue.use(x5GMaps, { key: 'AIzaSyD58iVDx399ThN50pkEl3kDHi5tGRH3XCU', libraries: ['places'] })
+Vue.use(x5GMaps, { key: '', libraries: ['places'] })
 
 Vue.config.productionTip = false
 
